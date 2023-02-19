807b036611da2c92a478bbccc15d7ea94b28335f
anita <ic22b055@fh-technikum.at>
2023-02-19 18:58:40 +0100

    SLMProjekt

---
+++ src/main/java/at/fhtw/bic/slmprojekt/ProjektController.java
@@ -0,0 +1,25 @@
+package at.fhtw.bic.slmprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProjektController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektController.class, args);
+    }
+
+    @GetMapping("/grade/{number}")
+    public String getGrade(@PathVariable int number) {
+        if (number == 00) return "00:00 AM";
+        else if (number <= 11) return number + "AM";
+        else if (number >= 13) return number + "PM";
+        else if (number == 12) return "12:00 PM";
+        else return "Not definiert";
+    }
+}
