7967f2a73a8e8c28f3261d8001b48c51b106807c
anita <ic22b055@fh-technikum.at>
2023-02-19 18:32:42 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/bic/slmprojekt/SlmProjektTadjikApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.slmprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmProjektTadjikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmProjektTadjikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/bic/slmprojekt/SlmProjektTadjikApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.slmprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmProjektTadjikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
